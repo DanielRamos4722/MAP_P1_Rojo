127253ba931446c277f8a846a107bd208f3918a1
Remshy <daniel.ramos.47@outlook.com>
2024-01-29 12:56:13 +0100

    commit clase 1

--- MAP_P1_Rojo_Unity/Assets/Scripts/OctorokManager.cs
+++ MAP_P1_Rojo_Unity/Assets/Scripts/OctorokManager.cs
@@ -1,13 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.PackageManager;
 using UnityEngine;
 
 public class OctorokManager : MonoBehaviour
 {
     Animator anim;
     Transform myTransform;
-    int dir = 0;
     float speed;
+    int dir;
+    bool orient;
 
     // Start is called before the first frame update
     void Start()
@@ -20,25 +22,46 @@ public class OctorokManager : MonoBehaviour
         else
             speed = 0.5f;
 
-        bool orient = true;
-
-        if(Random.RandomRange(0, 2) == 1)
-        {
-            orient = false;
-            dir++;
-            
-        }
-        anim.SetBool("Front", orient);
-
-        if (Random.RandomRange(0, 2) == 1)
-        {
-            myTransform.rotation = Quaternion.Euler(0, 0, 180);
-        }
+        StartCoroutine("SetDirection");
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void Move()
+    {
+        if (orient)
+        {
+
+        }
+    }
+
+    private IEnumerator SetDirection()
+    {
+        while(true)
+        {
+            dir = 0;
+            bool flip = false;
+            orient = true;
+
+            if (Random.RandomRange(0, 2) == 1)
+            {
+                orient = false;
+                dir++;
+            }
+            anim.SetBool("Front", orient);
+
+            if (Random.RandomRange(0, 2) == 1)
+            {
+                myTransform.rotation = Quaternion.Euler(0, 0, 180);
+                flip = true;
+                dir += 2;
+            }
+
+            yield return new WaitForSeconds(Random.RandomRange(0.5f, 1.1f));
+        }
     }
 }
