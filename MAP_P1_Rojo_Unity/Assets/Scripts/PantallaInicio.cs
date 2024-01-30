855096dc39f9eb32485696a962bd9458276554f7
julepage <julepage@ucm.es>
2024-01-30 21:23:05 +0100

    pantalla inicio

---
+++ MAP_P1_Rojo_Unity/Assets/Scripts/PantallaInicio.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Threading;
+using Unity.VisualScripting;
+using UnityEngine;
+using UnityEngine.UIElements;
+
+public class PantallaInicio : MonoBehaviour
+{
+    [SerializeField]
+    private float speed;
+    void Update()
+    {
+        transform.position += Vector3.down * speed * Time.deltaTime;
+    }
+
+    
+
+}
