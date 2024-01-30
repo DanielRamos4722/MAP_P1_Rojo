02b7db1f909dc45248e4684ee5971ab466ab4fa4
Remshy <daniel.ramos.47@outlook.com>
2024-01-30 18:19:29 +0100

    CAMARA1
    Cámara dinámica implementada.
    Falta cambiar el comportamiento de link en la transición.

---
+++ MAP_P1_Rojo_Unity/Assets/Scripts/CameraController.cs
@@ -0,0 +1,65 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    private float speed = 0.1f;
+    private Transform myTransform;
+    private BoxCollider2D boxColl;
+
+    [SerializeField] GameObject link;
+    InputManager inputManager;
+
+    float xDistance;
+    float yDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        float margen = 1f;
+        myTransform = transform;
+
+        boxColl = GetComponent<BoxCollider2D>();
+        boxColl.size = new Vector2((10f / Screen.height * Screen.width) - margen, 10 - margen);
+
+        xDistance = ((10f / Screen.height * Screen.width) / 2) - 0.2f;
+        yDistance = 4.8f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        StartCoroutine(Desplazamiento());
+    }
+
+    IEnumerator Desplazamiento()
+    {
+        Vector3 currentPos = myTransform.position;
+        Vector3 targetPos;
+
+        float xLink = link.transform.position.x - myTransform.position.x;
+        float yLink = link.transform.position.y - myTransform.position.y;
+
+        if (xLink > xDistance)
+            targetPos = currentPos + new Vector3(boxColl.size.x, 0, 0);
+        else if (xLink < -xDistance)
+            targetPos = currentPos + new Vector3(-boxColl.size.x, 0, 0);
+        else if (yLink > yDistance)
+            targetPos = currentPos + new Vector3(0, boxColl.size.y, 0);
+        else
+            targetPos = currentPos + new Vector3(0, -boxColl.size.y, 0);
+
+        while (myTransform.position != targetPos)
+        {
+            
+            myTransform.position = Vector3.MoveTowards(myTransform.position, targetPos, speed);
+            yield return null;
+        }
+    }
+}
