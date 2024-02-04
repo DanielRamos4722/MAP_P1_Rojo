f65388255f4bcadebf86f180de7919a0242a511c
julepage <julepage@ucm.es>
2024-02-04 21:47:28 +0100

    pantalla inicio

--- MAP_P1_Rojo_Unity/Assets/Scripts/PantallaInicio.cs
+++ MAP_P1_Rojo_Unity/Assets/Scripts/PantallaInicio.cs
@@ -8,13 +8,48 @@ using UnityEngine.UIElements;
 public class PantallaInicio : MonoBehaviour
 {
     [SerializeField]
-    private float speed = 1.2f;
-    void Update()
-    {
+    public float velocidadMovimiento = 1.2f;
+    public Vector3 primeraPosicionFinal = new Vector3(0f, -8.20f, 0f);
+    public Vector3 segundaPosicionFinal = new Vector3(0f, -77.5f, 0f);
+    public float tiempoDeEspera = 4f;
 
-        transform.position += Vector3.down * speed * Time.deltaTime;
+    void Start()
+    {
+        // Inicia la corrutina para mover la cámara hacia abajo
+        StartCoroutine(MoverYEsperar());
     }
 
-    
+    IEnumerator MoverYEsperar()
+    {
+        // Mueve la cámara hacia abajo hasta alcanzar la primera posición final
+        while (transform.position.y > primeraPosicionFinal.y)
+        {
+            transform.Translate(Vector3.down * velocidadMovimiento * Time.deltaTime);
+            yield return null;
+        }
 
+        // Espera durante 4 segundos en la primera posición final
+        yield return new WaitForSeconds(tiempoDeEspera);
+
+        // Mueve la cámara hacia abajo hasta alcanzar la segunda posición final
+        while (transform.position.y > segundaPosicionFinal.y)
+        {
+            transform.Translate(Vector3.down * velocidadMovimiento * Time.deltaTime);
+            yield return null;
+        }
+
+        // Espera durante 4 segundos en la segunda posición final
+        yield return new WaitForSeconds(tiempoDeEspera);
+
+        // Continúa moviendo la cámara hacia abajo en un bucle infinito
+        while (true)
+        {
+            transform.Translate(Vector3.down * velocidadMovimiento * Time.deltaTime);
+            yield return null;
+        }
+    }
 }
+
+
+
+
