5dfc03fa18cc619f73342bd1154adc32729e50e4
DaniRuizG <daniruizgopegui@gmail.com>
2024-02-05 19:01:03 +0100

    Scene manager por tercera vez

--- MAP_P1_Rojo_Unity/Assets/Scripts/SceneManager.cs
+++ MAP_P1_Rojo_Unity/Assets/Scripts/SceneManager.cs
@@ -2,16 +2,18 @@ using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class CambioDeEscena : MonoBehaviour
+public class ScnMnger : MonoBehaviour
 {
     void Update()
     {
         // Cambia a la Escena 2 al recibir cualquier input
-        if (Input.anyKeyDown)
+        if (SceneManager.GetActiveScene().name == "Pantalla start")
         {
-            CambiarAEscena2();
+            if (Input.anyKeyDown)
+            {
+                CambiarAEscena2();
+            }
         }
-
         // Cambia a la Escena 3 cuando la posición Y de la cámara es menor que -60 en la Escena 2
         if (SceneManager.GetActiveScene().name == "PantallaInicio" && Camera.main.transform.position.y < -60f)
         {
