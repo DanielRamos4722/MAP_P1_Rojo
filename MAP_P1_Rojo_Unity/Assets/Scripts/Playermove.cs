a4d7602582ac743dadf2f1e719eebbaf5503204a
Ramudaparty2 <sergio.yagojuarez@gmail.com>
2024-02-07 16:14:00 +0100

    Arreglé un problema del playermove que te impedía moverte si no tenías un mando

--- MAP_P1_Rojo_Unity/Assets/Scripts/Playermove.cs
+++ MAP_P1_Rojo_Unity/Assets/Scripts/Playermove.cs
@@ -28,9 +28,19 @@ public class Playermove : MonoBehaviour
     {
 
         var gamepad = Gamepad.current;
-        Vector2 movementinput = gamepad.leftStick.ReadValue();
-        direction = new Vector3(movementinput.x, movementinput.y, 0);
-         
+        if (gamepad != null) 
+        {
+            Vector2 movementinput = gamepad.leftStick.ReadValue();
+            if (movementinput.x == 0 && movementinput.y == 0)
+            {
+
+            }
+            else
+            {
+                direction = new Vector3(movementinput.x, movementinput.y, 0);
+            }
+        }
+        
         _transform.position += (direction * Time.deltaTime * speed);
       
     }
