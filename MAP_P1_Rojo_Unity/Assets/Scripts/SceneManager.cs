8cdcaa2556de283a4d21bed0f44f88a25f57105f
DaniRuizG <daniruizgopegui@gmail.com>
2024-02-11 12:08:24 +0100

    Empiezo a creer que se me borran los commits

--- MAP_P1_Rojo_Unity/Assets/Scripts/SceneManager.cs
+++ MAP_P1_Rojo_Unity/Assets/Scripts/SceneManager.cs
@@ -1,21 +1,24 @@
-using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class ScnMnger : MonoBehaviour
+public class ScnMng : MonoBehaviour
 {
+    void Start()
+    {
+        // Inicia en la Escena 1
+        //SceneManager.LoadScene("Pantalla start");
+    }
+
     void Update()
     {
         // Cambia a la Escena 2 al recibir cualquier input
-        if (SceneManager.GetActiveScene().buildIndex == 0)
+        if (Input.anyKeyDown)
         {
-            if (Input.anyKeyDown)
-            {
-                CambiarAEscena3();
-            }
+            CambiarAEscena2();
         }
+
         // Cambia a la Escena 3 cuando la posición Y de la cámara es menor que -60 en la Escena 2
-        if (SceneManager.GetActiveScene().buildIndex == 0 && Camera.main.transform.position.y < -60f)
+        if (SceneManager.GetActiveScene().name == "PantallaInicio" && Camera.main.transform.position.y < -60f)
         {
             CambiarAEscena3();
         }
@@ -23,11 +26,11 @@ public class ScnMnger : MonoBehaviour
 
     void CambiarAEscena2()
     {
-        SceneManager.LoadScene(0);
+        SceneManager.LoadScene("PantallaInicio");
     }
 
     void CambiarAEscena3()
     {
-        SceneManager.LoadScene(1);
+        SceneManager.LoadScene("MAP_P1_Rojo_Unity");
     }
 }
